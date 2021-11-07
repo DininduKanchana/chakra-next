284f5449233a4fc1be282acb66206cd4ef133fc7
Dinindu Kanchana <dinindukanchana@gmail.com>
2021-11-07 22:03:04 +0530

    size of three dot incresed

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -5,7 +5,7 @@ import { CardHeader } from "./card-header";
 
 export const Card = () => {
   return (
-    <Box w="60%" color="white" bg="#272B35" borderRadius="xl" m="5">
+    <Box w="640px" color="white" bg="#272B35" borderRadius="xl" m="5">
       <CardHeader status="Funding" userName="Ralph Edwards" imgUrl="/images/avatar.png" />
       <CardBody 
         description1="Velit ut ultrices quis viverra eu, ultricies nulla at nec. Ut diam venenatis egestas massa vulputate nam. Pretium eros, imperdiet odio sit. Natoque quam mi ut leo. Sed ut sit cursus nunc, sit. Magna neque vel amet sem vulputate lacus ut." 
